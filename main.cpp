45460084109ea64e5b3678561d61ae278769cb2c
David Newmon <david@localhost.localdomain>
2012-11-06 22:23:23 -0500

    -

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
-#include <iostream>
+#include "base.h"
+#include "socket.h"
 
 int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+	Socket *server = Socket::UdpSocket();
+	
+	sockaddr_in serverAddr;
+	bzero(&serverAddr, sizeof(serverAddr));
+	
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(53000);
+	
+	if (server->Bind((sockaddr*)&serverAddr, sizeof(serverAddr)) == 0) {
+		
+	} else {
+		cout << "Unable to bind!" << endl;
+	}
+	
+	delete server;
+	return 0;
 }
